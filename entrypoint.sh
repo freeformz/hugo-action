a4facf5471fd1bf88f3f1c6608769ced6b38d98a
Edward Muller <freeformz@gmail.com>
2019-06-23 15:38:58 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 echo "#################################################"
 echo "Starting the Hugo Action"
 
+git submodule init
 sh -c "GIT_COMMIT_SHA=`git rev-parse --verify HEAD` GIT_COMMIT_SHA_SHORT=`git rev-parse --short HEAD` hugo $*"
 
 echo "#################################################"
