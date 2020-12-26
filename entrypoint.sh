688ca6eb3d066c45f584c532a1e443e21abbe399
Edward Muller <edwardam@interlix.com>
2020-12-26 12:27:55 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ set -e
 echo "#################################################"
 echo "Starting the Hugo Action"
 
+hugo version
+
 git submodule init
 git submodule update
 sh -c "GIT_COMMIT_SHA=`git rev-parse --verify HEAD` GIT_COMMIT_SHA_SHORT=`git rev-parse --short HEAD` hugo $*"
